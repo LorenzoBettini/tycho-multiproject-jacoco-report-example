14ed9d6cc62b010f3307a661a54aca5c116e8e11
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-01-25 19:34:10 +0100

    plugin2 and plugin2.tests

---
+++ example.parent/example.plugin2.tests/src/example/plugin2/tests/MyClass2Test.java
@@ -0,0 +1,16 @@
+package example.plugin2.tests;
+
+import org.junit.Test;
+
+import example.plugin2.MyClass2;
+
+public class MyClass2Test {
+
+	private MyClass2 myClass = new MyClass2();
+
+	@Test
+	public void test() {
+		myClass.myMethod();
+	}
+
+}
---
+++ example.parent/example.plugin2/src/example/plugin2/MyClass2.java
@@ -0,0 +1,8 @@
+package example.plugin2;
+
+public class MyClass2 {
+
+	public void myMethod() {
+		System.out.println("hello");
+	}
+}
